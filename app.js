45b88b7e6152997afed9e96982f50f7d7b9b4418
Fauzi Mohd Ghouse <M.fauzi_87@hotmail.com>
2017-01-01 18:43:47 +0800

    added mongodb to app

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require('express')
+var app = express()
+var bodyParser = require('body-parser')
+var mongoose = require('mongoose')
+var Book = require('./Book.model')
+
+var db = 'mongodb://localhost:27017/example'
+mongoose.connect(db)
+
+var port = 8080
+
+app.listen(port, function () {
+  console.log('app listening to port ' + port)
+})
